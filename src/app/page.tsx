62bbb25a0f72822486f4b4341ec74f2d672d39ca
tjaec <87785559+tjaec@users.noreply.github.com>
2026-08-08 15:12:26 +0100

    feat: create reusable Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from "@/components/home/Hero";
+
 export default function Home() {
   return (
     <main>
-    <h1>Hello, I'm Thomas Cameron</h1>
-    <p>Computer Games Programmer</p>
-  </main>
+      <Hero />
+    </main>
   );
 }
---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>Hello, I'm Thomas Cameron</h1>
+      <p>Computer Games Programmer</p>
+    </section>
+  );
+}
